60b632869ec446887d2c033083e61ad2763c0a3f
Galbb811 <gal2274@gmail.com>
2025-03-17 12:50:57 -0400

    SS4_GalBB

---
+++ SS4_GalBB/sketch.js
@@ -0,0 +1,38 @@
+
+
+let Super;
+let normal;
+let T = 0;
+let T1 = 1000;
+let T2 = 8000;
+let w = 800;
+let h = 800;
+
+function preLoad()
+{
+Super = loadImage("Images/superwalk.jpg");
+normal = loadImage("Images/normalwalk.jpg");
+}
+
+function setup()
+{
+  createCanvas(800, 800);
+  imageMode(CENTER);
+  textAlign(CENTER);
+  textSize(20);
+}
+
+function draw() 
+{
+  background(220);
+  T = millis();
+
+   if (T > T2){
+    image(Super, w/2, h/2, w, h);
+    text("How I feel walking down the street with epic game music playing", w/2, 50);
+   }
+   else if (T > T1){
+    image(normal, w/2, h/2, w, h);
+    text("How I feel walking down the street with regualr music playing", w/2, 150);
+   }
+}
