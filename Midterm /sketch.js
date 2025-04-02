3b93d7a1d97fcd11b622f2328a10258a1eae6cfa
Galbb811 <gal2274@gmail.com>
2025-04-02 06:35:04 -0400

    Update sketch.js

--- Midterm /sketch.js
+++ Midterm /sketch.js
@@ -2,137 +2,120 @@
 // and you options to choose what to do, choose wisely or you may end up dead
 
 let scene = 1;
-let b1;
+let b1; 
 let b2;
-let b3;
+
 
 function setup() {
   createCanvas(900, 800);
+  rectMode(CENTER);
+  imageMode(CENTER);
   textAlign(CENTER);
   textSize(30);
 }
 
 function draw() {
-  console.log(mouseX)
 if (scene == 1){
     scene1();
-    b1 = cave;
-    b2 = forest;
-    b3 = ship;
   }
   if (scene == 2){
     scene2();
-    b1 = lookaround;
-    b2 = wayout;
+  
   }
   if (scene == 3){
     scene3();
-    b1 = scratch;
-    b2 = wayout;
+    
   }
   if (scene == 4){
     scene4();
-    b1 = swim;
-    b2 = supplies; 
+   
   }
   if (scene == 21){
     scene21();
-    b1 = freelegs;
-    b2 = think;
+    
   }
   if (scene == 22){
     scene22();
-    b2 = restart; 
+    
   }
   if (scene == 23){
     scene23();
-    b2 = restart; 
+   
   }
   if (scene == 24){
     scene24();
-    b2 = Gz;
+   
   }
   if (scene == 31){
     scene31();
-    b2 = restart;
+   
   }
   if (scene == 32){
     scene32();
-    b1 = follow;
-    b2 = treeline;
+   
   }
   if (scene == 33){
     scene33();
-    b2 = resetart; 
+   
   }
   if (scene == 34){
     scene34();
-    b2 = Gz;
+  
   }
   if (scene == 41){
     scene41();
-    b2 - restart;
+   
   }
   if (scene == 42){
     scene42();
-    b1 = flare;
-    b2 = cave2;
+    
   }
   if (scene == 43){
     scene43();
-    b2 = restart;
+   
   }
   if (scene == 44){
     scene44();
-    b2 = Gz;
+    
   }
-  //need a variable to store the value of button. thi svaribale is compared in if statement
-  //buttonString = b2.value()
-  ///if you wanted to switch from scene one when forest buttron is pressed:
-//   if (scene == 1 )
-//   {
-//     //if you are in scene one, then b2 is gonn abe labelled forest, when its pressed, lets move on to scene 3
-//   }
 }
 
 function mousePressed()
 {
-  if (mouseX > 153 && mouseX < 249 && mouseY > 602 && mouseY < 650 && scene == 1){
+  if (mouseX > 125&& mouseX < 225 && mouseY > 675 && mouseY < 725 && scene == 1 ){
    scene = 2;
   }
-  if (mouseX > 451 && mouseX < 547 && mouseY > 602 && mouseY < 650 && scene == 1){
+  else if (mouseX > 400 && mouseX < 500 && mouseY > 675 && mouseY < 725 && scene == 1){
     scene = 3;
-   }
-   if (mouseX > 751 && mouseX < 848 && mouseY > 602 && mouseY < 650 && scene == 1){
+}
+  if (mouseX > 725 && mouseX < 825 && mouseY > 625 && mouseY < 725 && scene == 1){
     scene = 4;
    }
-   if (mouseX > 175 && mouseX < 373 && mouseY > 602 && mouseY < 650 && scene == 2){
+   if (mouseX > 175 && mouseX < 375 && mouseY > 660 && mouseY < 740 && scene == 2){
     scene = 21;
    }
-   if (mouseX > 600 && mouseX < 798 && mouseY > 602 && mouseY < 650 && scene == 2){
+   if (mouseX > 175 && mouseX < 373 && mouseY > 602 && mouseY < 650 && scene == 2){
     scene = 22;
    }
-}
-
-
+   if (mouseX > 600 && mouseX < 798 && mouseY > 602 && mouseY < 650 && scene == 2){
+    scene = 23;
+   }
+//    if (mouseX > 600 && mouseX < 798 && mouseY > 602 && mouseY < 650 && scene == 21){
+//     scene = 23;
+// }
+  }
 function scene1() //starting scene
 { 
 background(200);
 text('welcome to Riveria', 450, 50);
 text('A world filled with magic, swords, and everything in between', 450, 100)
 text('Choose your starting point', 450, 150);
- cave = createButton('Cave');
- cave.style('font-size', '30px');
- forest = createButton('Forest');
- forest.style('font-size', '30px');
- ship = createButton('ship');
- ship.style('font-size', '30px');
- cave.position(150, 600);
- forest.position(450, 600);
- ship.position(750, 600);
- cave.size(100,50);
- forest.size(100,50);
- ship.size(100,50);
+rect(175, 700, 100, 50);
+rect(450, 700, 100, 50);
+rect(775, 700, 100, 50);
+text('Cave', 175, 710);
+text('Forest', 450, 710);
+text('Ship', 775, 710);
 }
 function scene2() //cave
 {
@@ -140,14 +123,11 @@ background(200);
 text('You wake up in dark cave', 450, 50);
 text('You notice glowing writing on the wall', 450, 100);
 text('What do you do?', 450, 150);
- lookaround = createButton('Look Around');
- lookaround.style('font-size', '30px');
- wayout = createButton('try to find a way out');
- wayout.style('font-size', '30px');
- lookaround.position(175, 600);
- wayout.position(600, 600);
- lookaround.size(200,80);
- wayout.size(200,80);
+rect( 275, 700, 200, 80);
+rect( 700, 700, 200, 80);
+text('Look Around', 275, 710);
+text('Try to find', 700, 695);
+text('a way out', 700, 725);
 } 
 function scene3() // forest
 { 
@@ -155,14 +135,12 @@ function scene3() // forest
   text('You wake up in a dense forest', 450, 50);
   text('You notice scratchs on the trees', 450, 100);
   text('What do you do?', 450, 150);
-  scratch = createButton('Investigate the scratches');
-  scratch.style('font-size', '30px');
-  wayout = createButton('Try to find a way out');
-  wayout.style('font-size', '30px');
-  scratch.position(175, 600);
-  wayout.position(600, 600);
-  scratch.size(200,80);
-  wayout.size(200,80);
+  rect( 275, 700, 200, 80);
+  rect( 700, 700, 200, 80);
+  text('Investigate', 275, 693);
+  text('the scratches', 275, 727);
+  text('Try to find', 700, 695);
+  text('a way out', 700, 725);
 }
 function scene4() //ship
 {
@@ -170,14 +148,12 @@ function scene4() //ship
   text('You wake up amongst the wreckege of a ship', 450, 50);
   text('The water is freezing', 450, 100);
   text('What do you do?', 450, 150);
-  swim = createButton('Swim to shore');
-  swim.style('font-size', '30px');
-  supplies = createButton('Look for supplies');
-  supplies.style('font-size', '30px');
-  swim.position(175, 600);
-  supplies.position(600, 600);
-  swim.size(200,80);
-  supplies.size(200,80);
+  rect( 275, 700, 200, 80);
+  rect( 700, 700, 200, 80);
+  text('Swim to', 275, 695);
+  text('shore', 275, 725);
+  text('Look for', 700, 695);
+  text('supplies', 700, 725);
 }
 function scene21() //looking around 
 {
@@ -186,14 +162,12 @@ function scene21() //looking around
   text('You realize that everything is covered in huge spiderwebs', 450, 100);
   text('and your legs are stuck', 450, 150);
   text ('What do you do?', 450, 200);
-  freelegs = createButton('Try to free your legs');
-  freelegs.style('font-size', '30px');
-  think = createButton('Try to think of a way out');
-  think.style('font-size', '30px');
-  freelegs.position(175, 600);
-  think.position(600, 600);
-  freelegs.size(200,80);
-  think.size(200,80);
+  rect( 275, 700, 200, 80);
+  rect( 700, 700, 200, 80);
+  text('Try to free', 275, 695);
+  text('your legs', 275, 725);
+  text('Think of a', 700, 695);
+  text('way out', 700, 725);
 }
 function scene22() //find a way out DEATH
 {
@@ -205,10 +179,8 @@ function scene22() //find a way out DEATH
   text('by doing so you alerted the spider who made the webs', 450, 250);
   text('As the spider sneaks behind you, it peirces you through your chest', 450, 300);
   text('Killing you instantly', 450, 350);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200,80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene23() //try to free yourself DEATH
 {
@@ -218,10 +190,8 @@ function scene23() //try to free yourself DEATH
   text('As you try to free your legs', 450, 150);
   text('the spider sneaks up behiend you and pierces through your chest', 450, 200);
   text('killing you instanty', 450, 250);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200,80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene24() //dont move and think
 {
@@ -232,10 +202,8 @@ function scene24() //dont move and think
   text('As soon as you read it, it vanishes, and your hands catch fire', 450, 200);
   text('Yet you dont burn, but everything else in the cave does', 450, 250);
   text ('You manage to escape the cave, after burnnig all of it', 450, 300);
-  Gz = createButton('Play Again');
-  Gz.style('font-size', '30px');
-  Gz.position(350, 600);
-  Gz.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Play Again', 450, 710);
 }
 function scene31() //investigate the scraths Death
 {
@@ -246,10 +214,8 @@ function scene31() //investigate the scraths Death
   text('Suddenly you feel a burning pain across your chest', 450, 200);
   text('What seems to be a werewolf, slashed you', 450, 250);
   text('Your vision fades to black as the werewolf closes in', 450,300);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene32() // find a way out
 {
@@ -259,27 +225,23 @@ text('You decide not to stay and find out who or what made them', 450, 100);
 text('You walk away, towards what seems like the edge of the tree line', 450, 150);
 text('Suddenly you hear some voices urging you to come closer', 450, 200);
 text('What do you do', 450, 250);
-follow = createButton('Follow the voices');
-follow.style('font-size', '30px');
-treeline = createButton('Ignore the voices and head for the tree line');
-treeline.style('font-size', '30px');
-follow.position(175, 600);
-treeline.position(600, 600);
-follow.size(200, 80);
-treeline.size(200, 80);
+rect( 275, 700, 200, 80);
+rect( 700, 700, 200, 80);
+text('Follow the', 275, 695);
+text('voices', 275, 725);
+text('Ignore the', 700, 695);
+text('voices', 700, 725);
 }
 function scene33() //follow the voices Death
 {
   background(200);
   text('You Are Dead', 450, 50);
   text('You follow the voices you hear luring you away from the tree line', 450, 100);
-  text('As you reach the voices, you see 3 beatiful women welcoming you', 450, 150);
+  text('As you reach the voices, you see 3 beautiful women welcoming you', 450, 150);
   text('As you draw closer Suddenly the faces of the three women', 450, 200);
   text('twist and contort, as they devour you whole', 450, 250);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene34() //ignore the voices 
 {
@@ -287,10 +249,8 @@ function scene34() //ignore the voices
   text('You deciede to ignore the voices, and head for the tree line', 450, 50);
   text('You manage to leave the forest', 450, 100);
   text('You spot a road leading to a city', 450, 150);
-  Gz = createButton('Play Again');
-  Gz.style('font-size', '30px');
-  Gz.position(350, 600);
-  Gz.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Play Again', 450, 710);
 }
 function scene41() //Swim to shore Death
 {
@@ -299,10 +259,8 @@ function scene41() //Swim to shore Death
   text('You try to swim to shore so you pick a direction and swim', 450, 100);
   text('But the freezing water soon overtakes you', 450, 150);
   text('You freeze to death and sink to the depths', 450, 200);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene42() //look for supplies
 {
@@ -312,14 +270,12 @@ function scene42() //look for supplies
   text('You resurface from the wreckage', 450, 150);
   text('You find an iceberg you can climb onto, with a cave', 450, 200);
   text('What do you do?', 450, 250);
-  flare = createButton('Shoot one of the flares');
-  flare.style('font-size', '30px');
-  cave2 = createButton("Explore the cave");
-  cave2.style('font-size', '30px');
-  flare.position(175, 600);
-  cave2.position(600, 600);
-  flare.size(200, 80);
-  cave2.size(200, 80);
+  rect( 275, 700, 200, 80);
+  rect( 700, 700, 200, 80);
+  text('Shoot one of', 275, 695);
+  text('the flares', 275, 725);
+  text('Explore the', 700, 695);
+  text('cave', 700, 725);
 }
 function scene43() // shoot the flares Death
 {
@@ -328,10 +284,8 @@ function scene43() // shoot the flares Death
   text('You decide to shoot one of your flares', 450, 100);
   text('The flare you shot alerted nearby predators', 450, 150);
   text('They close in on you, ready to pounce and eat you', 450, 200);
-  restart = createButton('Try Again');
-  restart.style('font-size', '30px');
-  restart.position(350, 600);
-  restart.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Try Again', 450, 710);
 }
 function scene44() //explore the cave
 {
@@ -341,8 +295,6 @@ function scene44() //explore the cave
   text('You examine the writing', 450, 150);
   text('Your vision goes blurry and everything fades to white', 450, 200);
   text('Your vision returns, but you seem to be in a city now', 450, 250);
-  Gz = createButton('Play Again');
-  Gz.style('font-size', '30px');
-  Gz.position(350, 600);
-  Gz.size(200, 80);
+  rect( 450, 700, 200, 80);
+  text('Play Again', 450, 710);
 }
