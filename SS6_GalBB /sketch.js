435769e320388d47ac7134f64ba4c92c46feecd0
Galbb811 <gal2274@gmail.com>
2025-04-23 14:33:05 -0400

    Update sketch.js

--- SS6_GalBB /sketch.js
+++ SS6_GalBB /sketch.js
@@ -36,8 +36,35 @@ function draw() {
   background(220);
   
 if (keyCode == 66){
-  image(pic[1], width/2, height/2, 500, 500)
-  sound[0].play()
+  image(pic[0], width/2, height/2, 500, 500);
+  sounds[0].play();
 }
- 
+if (keyCode == 83){
+  image(pic[1], width/2, height/2, 500, 500);
+  sound[1].play();
+}
+if (keyCode == 87){
+  image(pic[2], width/2, height/2, 500, 500);
+  sound[2].play();
+}
+if (keyCode == 69){
+  image(pic[3], width/2, height/2, 500, 500);
+  sound[3].play();
+}
+if (keyCode == 70){
+  image(pic[4], width/2, height/2, 500, 500);
+  sound[4].play();
+}
+if (keyCode == 67){
+  image(pic[5], width/2, height/2, 500, 500);
+  sound[5].play();
+}
+if (keyCode == 82){
+  image(pic[6], width/2, height/2, 500, 500);
+  sound[6].play();
+}
+if (keyCode == 72){
+  image(pic[7], width/2, height/2, 500, 500);
+  sound[7].play();
+}
 }
