6f1f131d575e644600076295c02482c0bad442ef
Galbb811 <gal2274@gmail.com>
2025-04-23 15:26:11 -0400

    SS5_GalBB

--- SS2_GalBB /sketch.js
+++ SS2_GalBB /sketch.js
@@ -1,99 +1,89 @@
 
-/*  Press e to draw the ears, h to draw the head, i to draw the eyes, no to draw the nose,
-w to draw the whiskers, m to draw the mouth, and t to draw the teeth
-posution your mouse on any point of the canvas to chnage the size and position
-of a new drwaing withe they keys mentioned before*/
+let r = 0
+let g = 0
+let b = 0
 
 function setup() {
-  createCanvas(800, 800);
-  background(220)
+  createCanvas(1000, 1000);
+  textAlign(CENTER);
+  textSize(30);
 }
 
-let x = 600
-let y = 400
 
 
  function draw() {
-  if (mouseX < width/2)[
-    x -= 0.2
-  ]
-  if (mouseY < height/2)[
-    y -= 0.2
-  ]
-  if (mouseX > width/2)[
-    x += 0.2
-  ]
-  if (mouseY > height/2)[
-    y += 0.2
-  ]
+  background(220);
+
+  text('WARNING', width/2, height - 900);
+  text('If you are Epileptic, please close your eyes', width/2, height - 800);
+  text("Press Space to begin", width/2, height - 700);
+
+  if( keyCode ==32 ){
+for(let x = 0; x < width; x = x + random(0, 600)){
+ for(let y = 0; y < height; y = y + random(0, 400)){
+//left ear
+fill(r, g, b);
+triangle(y+25, y/4, y+25, y-100, x+75, x/2);
+//L ear inside
+fill(r +20, g+20, b+20);
+triangle(y+50, y-225, y+75, x/2, x+75, x/2);
+
+//right ear
+fill(r, g, b);
+triangle(x+175, y/4, x+175, x/2, x-50, x/2);
+//R ear inside 
+fill(r-20, g-20, b-20);
+triangle(x+150, y-225, x+125, x/2, x-25, x/2);
+
+//head
+fill(r+100, g+100, b+100);
+ellipse(x, y, y, y);
+
+//left eye
+fill(r+50, g+50, b+50);
+ellipse(x-80, x/2, x/10, x/10 -10);
+//L pupil
+fill(r-25, g-150, b-200);
+ellipse(x-80, x/2, y/40, y/10 +5);
+//right eye
+fill(r+200, b+200, g+200);
+ellipse(x+75, x/2, x/10, x/10 - 10);
+//R pupil
+fill(r-200, b-200, g-200);
+ellipse(x+75, x/2, y/40, y/10 +5);
+
+//nose
+line(x, y+20, x+20, y);
+line(x, y+20, x-20, y);
+
+//Whiskers 
+line(x+25, y+5, x+100, y-20);
+line(x+25, y+10, x+100, y+10);
+line(x+25, y+15, x+100, y+40);
+line(x-25, y+5, y+100, y-20);
+line(x-25, y+10, y+100, y+10);
+line(x-25, y+15, y+100, y+40);
+
+//mouth
+fill(r, g-125, b-125);
+ellipse(x, y+75, y-250, x/10 -10);
+
+//teeth
+fill(r+255, g+255, b+255);
+triangle(x-60, y+60, x-50, y+80, x-40, y+55);
+triangle(x+40, y+55, x+50, y+80, x+60, y+60);
+triangle(x-50, y+92, x-40, y+75, x-30, y+97);
+triangle(x+30, y+97, x+40, y+75, x+50, y+92);
+
+if(x < width/2 && y < height/2){
+  r = random(0, 255);
+  g = random(0, 255);
+  b = random(0, 255);
 }
-
-function keyPressed(){
-  if (keyCode == 69){
-  //left ear
-  fill(129, 129, 129);
-  triangle(y+25, y/4, y+25, y-100, x+75, x/2);
-  //L ear inside
-  fill(100, 100, 100);
-  triangle(y+50, y-225, y+75, x/2, x+75, x/2);
-
-  //right ear
-  fill(129, 129, 129);
-  triangle(x+175, y/4, x+175, x/2, x-50, x/2);
-  //R ear inside 
-  fill(100, 100, 100);
-  triangle(x+150, y-225, x+125, x/2, x-25, x/2);
- } 
-
- if (keyCode == 72){
-  //head
-  fill(129, 129, 129);
-  ellipse(x, y, y, y);
  }
+  }
+   }
+    }
+
+
   
- if (keyCode == 73){
-  //left eye
-  fill(255, 220, 0);
-  ellipse(x-80, x/2, x/10, x/10 -10);
-  //right pupil
-  fill(0, 0, 0);
-  ellipse(x-80, x/2, y/40, y/10 +5);
- //right eye
- fill(255, 220, 0);
-  ellipse(x+75, x/2, x/10, x/10 - 10);
-  // right pupil
-  fill(0, 0, 0);
-  ellipse(x+75, x/2, y/40, y/10 +5);
- }
-
- if (keyCode == 78){
-  //nose
-  line(x, y+20, x+20, y);
-  line(x, y+20, x-20, y);
- }
-
- if (keyCode == 87){
-  //Whiskers 
-  line(x+25, y+5, x+100, y-20);
-  line(x+25, y+10, x+100, y+10);
-  line(x+25, y+15, x+100, y+40);
-  line(x-25, y+5, y+100, y-20);
-  line(x-25, y+10, y+100, y+10);
-  line(x-25, y+15, y+100, y+40);
- }
-
- if (keyCode == 77){
-  //mouth
-  fill(189, 57, 18);
-  ellipse(x, y+75, y-250, x/10 -10);
- }
-
- if (keyCode == 84){
-  //teeth
-  fill(255, 255, 255);
-   triangle(x-60, y+60, x-50, y+80, x-40, y+55);
-   triangle(x+40, y+55, x+50, y+80, x+60, y+60);
-   triangle(x-50, y+92, x-40, y+75, x-30, y+97);
-  triangle(x+30, y+97, x+40, y+75, x+50, y+92);
- }
-}
